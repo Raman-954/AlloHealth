7842cc4d560ed7af985cfdd6d70cd67e18fbf821
Raman-954 <152240479+Raman-954@users.noreply.github.com>
2026-05-13 00:21:06 +0530

    Update route.ts

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,12 +1,16 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+
 export async function GET() {
   const products = await prisma.product.findMany({
     include: {
       inventories: {
-        include: { warehouse: true }
-      }
-    }
-  }); 
-  return NextResponse.json();
+        include: {
+          warehouse: true,
+        },
+      },
+    },
+  });
+
+  return NextResponse.json(products);
 }
